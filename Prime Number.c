f954cac88a413c3f5765af86b73113a82dfd5a04
CodenameREBIRTH <rebirthakg@gmail.com>
2020-10-01 02:42:00 +0530

    Modified codes

--- Prime Number.c
+++ Prime Number.c
@@ -1,15 +1,24 @@
-int main(){
+#include<stdio.h>
+
+int main()
+{
 	int i,j,n,m; 
 	scanf("%d",&n);
-	for(i=1;i<=n;i++){
-	    m=0;
-	    for(j=1;j<=n;j++)
-	    {
-	    if(i%j==0)
-	    m++;
-	    }
+	for(i=1;i<=n;i++)
+	{
+	   m=0;
+	   for(j=1;j*j<=i;j++)
+	   {
+				if(i%j==0)
+				{
+					if(j==i/j)
+						m++;
+					else
+						m+=2;
+				}
+			}
 	    if(m==2)
-	    printf("%d ",i);
+	    printf("%d\n",i);
 	}
-return 0;
+	return 0;
 }
