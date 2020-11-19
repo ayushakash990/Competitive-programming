a9b0e102f0d8a234cadda8620fed457167235fd0
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2020-11-19 15:58:25 +0530

    Create Goki and his breakup.cpp

---
+++ Goki and his breakup.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int n,min; cin>>n>>min;
+      while(n--)
+      {
+         int k; cin>>k;
+         if(k>=min) cout<<"YES\n";
+         else cout<<"NO\n";
+      }
+    
+
+}
