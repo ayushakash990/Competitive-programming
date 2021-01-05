458c443544e22dc67e6b63bf30f71ac5cbfaca72
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-01-05 08:59:19 +0530

    Create Rajan and Odd frequency number.cpp

---
+++ Rajan and Odd frequency number.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  int n,x,res=0; cin>>n; n=(n<<1)+1; while(n--){ cin>>x; res^=x; }
+  cout<<res;
+  return 0;
+}
