41f52c13a6d280cf49e74124b5d48c1e3e2bcb7b
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-01-18 09:23:27 +0530

    Create Lift queries.cpp

---
+++ Lift queries.cpp
@@ -0,0 +1,13 @@
+// BY AYUSH AKASH
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,a=0,b=7; cin>>t;
+    while(t--){
+        int x; cin>>x;
+        if(abs(b-x)<abs(x-a)){ b=x; cout<<"B\n"; }
+        else{ a=x; cout<<"A\n"; }
+    }
+    return 0;
+}
