fc5f1c5568d83e1495a378194972235411f9280a
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-01-18 09:28:42 +0530

    Create Palindromic String.cpp

---
+++ Palindromic String.cpp
@@ -0,0 +1,12 @@
+// BY AYUSH AKASH
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str; cin>>str;
+    string strn(str);
+    reverse(str.begin(),str.end());
+    if(str==strn) cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}
