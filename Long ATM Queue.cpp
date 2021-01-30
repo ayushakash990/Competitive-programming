8bd5a14e412d91521c54af5908af689eeb122ff2
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-01-30 10:16:08 +0530

    Create Long ATM Queue.cpp

---
+++ Long ATM Queue.cpp
@@ -0,0 +1,11 @@
+// BY AYUSH AKASH
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+  int n,x,p=0,res=1; cin>>n;
+  while(n--){ cin>>x; if(x<p) res++; p=x; }
+  cout<<res;
+  return 0;
+}
