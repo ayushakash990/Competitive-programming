d03aa549e1cdfec63ef0e01738523b15c88e35f0
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-02-09 12:10:40 +0530

    Create super reduced strings.py

---
+++ super reduced strings.py
@@ -0,0 +1,19 @@
+// BY AYUSH AKASH
+s = input()
+stack = [s[0]]
+for i in range(1,len(s)):
+    if stack:
+        
+        if stack[-1]== s[i]:
+            #print("ssds",stack)
+            stack.pop()
+        else:
+            stack.append(s[i])
+    else:
+
+        stack.append(s[i])
+    #stack.append(s[i])
+if len(stack)>0:
+    print(''.join(stack))
+else:
+    print('Empty String')
