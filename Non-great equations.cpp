4c1886a03e60cdbb7d4458dda0a2bc9298dc540e
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-02-15 08:43:24 +0530

    Create Non-great equations.cpp

---
+++ Non-great equations.cpp
@@ -0,0 +1,16 @@
+// BY AYUSH AKASH
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long a, b, c;
+	cin >> b;
+
+	c = (b*b + b)/2;
+	a = (b*b - b)/2;
+
+	cout << a << " " << c << "\n";
+	return 0;
+}
