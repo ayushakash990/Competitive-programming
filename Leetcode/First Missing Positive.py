d89631fe5c347a6d0a7f68c30d7298852d0796bc
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-10-10 08:15:18 +0530

    Create First Missing Positive.py

---
+++ Leetcode/First Missing Positive.py
@@ -0,0 +1,14 @@
+Link-https://leetcode.com/problems/first-missing-positive/
+class Solution(object):
+    def firstMissingPositive(self, A):
+        i = 0
+        while i < len(A):
+            if A[i] > 0 and A[i] - 1 < len(A) and A[i] != A[A[i]-1]:
+                A[A[i]-1], A[i] = A[i], A[A[i]-1]
+            else:
+                i += 1
+
+        for i, integer in enumerate(A):
+            if integer != i + 1:
+                return i + 1
+        return len(A) + 1
