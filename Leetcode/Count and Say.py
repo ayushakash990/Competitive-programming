1babf28e2325c0aa7f35d590cca9380d1d81af44
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2021-10-10 08:12:03 +0530

    Create Count and Say.py

---
+++ Leetcode/Count and Say.py
@@ -0,0 +1,19 @@
+Link-https://leetcode.com/problems/count-and-say/
+class Solution(object):
+    # @return a string
+    def countAndSay(self, n):
+        seq = "1"
+        for i in xrange(n - 1):
+            seq = self.getNext(seq)
+        return seq
+
+    def getNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            cnt = 1
+            while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+                cnt += 1
+                i += 1
+            next_seq += str(cnt) + seq[i]
+            i += 1
+        return next_seq
